12997584c95c47e97347ae365def621dfe0e2de6
papithap913 <166851777+papithap913@users.noreply.github.com>
2024-11-17 07:40:12 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -27,7 +27,7 @@ const Navbar = () => {
               </Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="/properties">
+              <Link className="nav-link" to="/PropertyList">
                 Properties
               </Link>
             </li>
