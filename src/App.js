981731fea2de9a87632ea764131c8b2086957ae9
papithap913 <166851777+papithap913@users.noreply.github.com>
2024-11-18 13:28:28 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Login from "./components/Login";
 import AgentPortal from "./components/AgentPortal";
 import Register from "./components/Register";
 import AddProperty from "./components/AddProperty";
-import properties from "./components/properties";
+import properties from "./components/PropertyList";
 import EditProperty from "./components/EditProperty";
 import './App.css';
 
@@ -21,7 +21,7 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/agents" element={<AgentPortal />} />
         <Route path="/addproperty" element={<AddProperty />} />
-        <Route path="/properties" element={<properties />} />
+        <Route path="/properties" element={<PropertyList />} />
         <Route path="/editproperty/:id" element={<EditProperty />} />
       </Routes>
     </Router>
